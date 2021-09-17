ba8d239114f13afb76c2712068ff35bf26250d61
Daniil Boyko <daniil09052000@mail.ru>
2021-09-17 15:56:16 +0300

    created a table, added pagination and table search

--- datum-test/src/components/molecules/Table.vue
+++ datum-test/src/components/molecules/Table.vue
@@ -1,6 +1,43 @@
 <template>
 	<div>
-    <b-table striped hover :items="items"></b-table>
+		<b-form-group
+			label="Поиск"
+			label-for="filter-input"
+			label-cols-sm="3"
+			label-align-sm="right"
+			label-size="sm"
+			class="mb-0"
+		>
+			<b-input-group size="sm">
+				<b-form-input
+					id="filter-input"
+					v-model="filter"
+					type="search"
+					placeholder="Введите для поиска"
+				></b-form-input>
+
+				<b-input-group-append>
+					<b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
+				</b-input-group-append>
+			</b-input-group>
+		</b-form-group>
+		<p class="mt-3">Текущая страница: {{ currentPage }}</p>
+    <b-table striped hover 
+			:items="items" 
+			id="my-table" 
+			:per-page="perPage" 
+			:current-page="currentPage"
+			:filter="filter"
+			:filter-included-fields="filterOn"
+			@filtered="onFiltered"
+			>
+		</b-table>
+		<b-pagination
+			v-model="currentPage"
+			:total-rows="totalRows"
+			:per-page="perPage"
+			aria-controls="my-table"
+		></b-pagination>
   </div>
 </template>
 
@@ -8,12 +45,35 @@
 export default {
 	name: 'Table',
 	data:() =>({
+		perPage: 6,
+		totalRows: 1,
+		currentPage: 1,
+		filter: null,
+		filterOn: [],
     items: [
 			{ age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
 			{ age: 21, first_name: 'Larsen', last_name: 'Shaw' },
 			{ age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-			{ age: 38, first_name: 'Jami', last_name: 'Carney' }
+			{ age: 21, first_name: 'Daniil', last_name: 'Daniilov' },
+			{ age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+			{ age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+			{ age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+			{ age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+			{ age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+			{ age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+			{ age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+			{ age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+			{ age: 89, first_name: 'Geneva', last_name: 'Wilson' },
 		]
   }),
+	mounted() {
+		this.totalRows = this.items.length
+	},
+	methods: { 
+		onFiltered(filteredItems) {
+			this.totalRows = filteredItems.length
+			this.currentPage = 1
+		}
+	}
 }
 </script>
--- datum-test/src/views/Statement.vue
+++ datum-test/src/views/Statement.vue
@@ -1,21 +1,13 @@
 <template>
 	<div class="container">
 		<h1 class="mt-5">Журнал</h1>
-		<b-container class="bv-example-row">
-			<b-row>
-				<b-col>
-					<Table />
-				</b-col>
-				<b-col>
-					Тут форма
-				</b-col>
-			</b-row>
+		<b-container class="bv-example-row mt-2">
+			<Table />
 		</b-container>
 	</div>
 </template>
 
 <script>
-import Table from '../components/molecules/Table.vue'
 export default {
   name: 'statement',
 	components: {
